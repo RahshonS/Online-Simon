1b8ec1eefce2c24fd8efa1d114d2098f3a83bae9
Rahshon <tuf70447@temple.edu>
2021-12-19 01:04:23 -0800

    Introduced code that allows the creation of randomly generated arrays each time which allows for a random sequence of lights to be triggered each game. Some code removed and cleaned up.

--- simon.js
+++ simon.js
@@ -8,48 +8,6 @@ let testArray = [0,2,3,1,2];
 
 let maxLevel = 5;
 
-for(let i = 0; i < maxLevel; i++){
-    let currLevel = i + 1;
-    console.log("LEVEL:" + currLevel);
-
-
-    for(let k = 0; k <= i; k++){
-        //grab a number from the test array... use this to trigger the appropriate light
-        //make sure we stay within the test array bounds
-        
-        currLight = testArray[k];
-        //console.log("Test Array Num is: " + currLight);
-
-        //Plug the number (currLight) into the light array to trigger the appropriate light
-        let colorToTrigger = light[currLight];
-
-        console.log(colorToTrigger);
-    }
-    
-    
-}
-
-//RANDOM NUMBER GENERATOR LOGIC
-/*
-let ranNum = Math.random();
-ranNum = (ranNum * 10);
-console.log("Random num is: " + ranNum);
-ranNum = Math.ceil(ranNum);
-console.log("Random num (ceiling) is: " + ranNum);
-ranNum = ranNum % 5;
-console.log("Random num:" + ranNum);
-*/
-
-/*
-if(ranNum == 4){
-    console.log("Random num is Zero");
-}
-else{
-    console.log("Random num:" + ranNum);
-}*/
-
-console.log("----RANDOM RUN----");
-
 //let arrSize = 5;
 let ranArray= [];
 
@@ -59,7 +17,27 @@ for(let n = 0; n < 5; n++){
     ranArray[n] = ranNum;
 }
 
-console.log("Array: " + ranArray);
+console.log("Rand Array: " + ranArray);
+
+for(let i = 0; i < maxLevel; i++){
+    let currLevel = i + 1;
+    console.log("LEVEL:" + currLevel);
+
+
+    for(let k = 0; k <= i; k++){
+        //grab a number from the test array... use this to trigger the appropriate light
+        //make sure we stay within the test array bounds
+        
+        currLight = ranArray[k];
+        //console.log("Test Array Num is: " + currLight);
+
+        //Plug the number (currLight) into the light array to trigger the appropriate light
+        let colorToTrigger = light[currLight];
+
+        console.log(colorToTrigger);
+    }
+}
+
 
 function genRandomNum(){
     let ranNum = Math.random();
